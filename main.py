6dfd88384d3cd222704b83f29a1d1bcee73b7ecd
DanielMerser1 <91954962+M2101479@users.noreply.github.com>
2021-11-29 09:00:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# hash for integer unchanged
+print('Hash for 200 is:', hash(200))
+
+# hash for decimal
+print('Hash for 200.123 is:',hash(200.123))
+
+# hash for string
+print('Hash for word is:', hash('word'))
+# tuple of vowels
+vowels = ('a', 'e', 'i', 'o', 'u')
+
+print('The hash is:', hash(vowels))
